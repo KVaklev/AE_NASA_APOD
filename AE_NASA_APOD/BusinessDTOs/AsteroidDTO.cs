6303cc92e2ce196602535da6ca37f16a91edcb0d
Kristian Vaklev <kristian.vaklev@yahoo.com>
2023-09-03 18:09:35 +0300

    Added LayeredStructure - Business, DataAccess and Presentation

---
+++ AE_NASA_APOD/BusinessDTOs/AsteroidDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace BusinessDTOs
+{
+    public class AsteroidDTO
+    {
+
+    }
+}
---
+++ AE_NASA_APOD/BusinessExceptions/DuplicateEntityException.cs
@@ -0,0 +1,10 @@
+﻿namespace BusinessExceptions
+{    
+    public class DuplicateEntityException : ApplicationException
+    {
+        public DuplicateEntityException(string message)
+            : base(message)
+        {
+        }
+    }
+}
