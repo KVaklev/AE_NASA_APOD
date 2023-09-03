ac1a9108b6f48cd71ab63fff7dc667906795cd1a
Kristian Vaklev <kristian.vaklev@yahoo.com>
2023-09-03 19:04:52 +0300

    DataAccess and Business Layer Skeleton Structure Added

--- AE_NASA_APOD/DataAccessRepositories/Contracts/IAsteroidRepository.cs
+++ AE_NASA_APOD/DataAccessRepositories/Contracts/IAsteroidRepository.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccessRepositories.Models
+namespace DataAccessRepositories.Contracts
 {
-    internal class AsteroidRepositories
+    public interface IAsteroidRepository
     {
     }
 }
--- AE_NASA_APOD/DataAccessRepositories/Models/AsteroidRepository.cs
+++ AE_NASA_APOD/DataAccessRepositories/Models/AsteroidRepository.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccessRepositories.Contracts
+namespace DataAccessRepositories.Models
 {
-    internal interface IAsteroidRepositories
+    public class AsteroidRepository
     {
     }
 }
