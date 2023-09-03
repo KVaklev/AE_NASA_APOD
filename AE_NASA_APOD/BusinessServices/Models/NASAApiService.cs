4c41321f809fccf935832e62c8af6e22fa775c36
Kristian Vaklev <kristian.vaklev@yahoo.com>
2023-09-03 18:47:55 +0300

    NASA API Interface and Service Initiated

---
+++ AE_NASA_APOD/BusinessServices/Models/NASAApiService.cs
@@ -0,0 +1,31 @@
+﻿using BusinessServices.Contracts;
+using DataAccessModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessServices.Models
+{
+    public class NASAApiService : INASAApiService
+    {
+        private readonly HttpClient httpClient;
+
+        public NASAApiService()
+        {
+            this.httpClient = new HttpClient();
+            this.httpClient.BaseAddress = new Uri("https://api.nasa.gov/planetary/apod");
+        }
+
+        public Task<APOD> GetAPODAsync(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Asteroid>> GetAsteroidsAsync(DateTime startDate, DateTime endDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
