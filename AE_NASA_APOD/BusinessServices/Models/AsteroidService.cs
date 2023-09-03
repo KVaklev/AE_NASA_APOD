679fa2d1187cbf0539baf473de78e4101070dd7c
Kristian Vaklev <kristian.vaklev@yahoo.com>
2023-09-03 19:44:10 +0300

    AsteroidService Implements IAsteroidService with methods definition and implementation

--- AE_NASA_APOD/BusinessServices/Models/AsteroidService.cs
+++ AE_NASA_APOD/BusinessServices/Models/AsteroidService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessServices.Contracts;
+using DataAccessModels.Models;
+using DataAccessRepositories.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,27 @@ using System.Threading.Tasks;
 
 namespace BusinessServices.Models
 {
-    public class AsteroidService
+    public class AsteroidService : IAsteroidService
     {
+        private readonly IAsteroidRepository asteroidRepository;
+
+        public AsteroidService (IAsteroidRepository asteroidRepository)
+        {
+            this.asteroidRepository = asteroidRepository;
+        }
+        public List<Asteroid> GetAll()
+        {
+            return this.asteroidRepository.GetAll();
+        }
+        public Asteroid GetById(int id)
+        {
+            return this.asteroidRepository.GetById(id);
+        }
+        public List<Asteroid> FilterBy(AsteroidQueryParameters asteroidQueryParameters)
+        {
+            throw new NotImplementedException();
+        }
+
 
     }
 }
