35fb25fc51b3302013c48ab7042bc9559719db21
Kristian Vaklev <kristian.vaklev@yahoo.com>
2023-09-03 18:39:57 +0300

    AsteroidModel and Relevant Properties Added

---
+++ AE_NASA_APOD/BusinessServices/Contracts/INASAApiService.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessServices.Contracts
+{
+    public interface INASAApiService
+    {
+        Task<List<Asteroid>> GetAsteroidsAsync(DateTime startDate, DateTime endDate);
+        Task<APOD> GetAPODAsync(DateTime date);
+
+    }
+}
--- AE_NASA_APOD/DataAccessModels/Models/Asteroid.cs
+++ AE_NASA_APOD/DataAccessModels/Models/Asteroid.cs
@@ -8,5 +8,14 @@ namespace DataAccessModels.Models
 {
     public class Asteroid
     {
+        public string Copyright { get; set; }
+        public DateTime Date { get; set; }
+        public string Explanation { get; set; }
+        public string HdUrl { get; set; }
+        public string MediaType { get; set; }
+        public string ServiceVersion { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+
     }
 }
