c17e55c0941054902f6c599386f1c168f9c7d3b5
Kristian Vaklev <kristian.vaklev@yahoo.com>
2023-09-05 11:33:23 +0300

    Details Action With Implementation in AsteroidController and Details View Added

--- AE_NASA_APOD/AE_NASA_APOD/Controllers/MVC/AsteroidController.cs
+++ AE_NASA_APOD/AE_NASA_APOD/Controllers/MVC/AsteroidController.cs
@@ -1,7 +1,9 @@
-﻿using BusinessQueryParameters;
+﻿using BusinessExceptions;
+using BusinessQueryParameters;
 using BusinessServices.Contracts;
 using DataAccessModels.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 
 namespace AE_NASA_APOD.Controllers.MVC
 {
@@ -10,17 +12,35 @@ namespace AE_NASA_APOD.Controllers.MVC
         private readonly IAsteroidService asteroidService;
 
 
-        public AsteroidController (IAsteroidService asteroidService)
+        public AsteroidController(IAsteroidService asteroidService)
         {
             this.asteroidService = asteroidService;
         }
 
         [HttpGet]
-        public IActionResult Index(AsteroidQueryParameters asteroidQueryParameters)
+        public IActionResult Index([FromQuery] AsteroidQueryParameters asteroidQueryParameters)
         {
             List<Asteroid> asteroids = this.asteroidService.FilterBy(asteroidQueryParameters);
 
             return View(asteroids);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            try
+            {
+                Asteroid asteroid = this.asteroidService.GetById(id);
+
+                return View(asteroid);
+            }
+            catch (EntityNotFoundException e)
+            {
+                this.HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+                this.ViewData["ErrorMessage"] = e.Message;
+
+                return this.View("Error");
+            }
         }
     }
 }
